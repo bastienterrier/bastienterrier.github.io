7c4211cafea41743cf99a16a7c6b5cd8d829627c
Bastien TERRIER <bastien.terrier@gmail.com>
2021-09-30 23:20:28 +0200

    feat: add CV content (#1)

--- README.md
+++
@@ -1,3 +0,0 @@
-# Welcome
-
-🚧 Work in progress 👷
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    content: ["index.html"],
+    options: {
+      keyframes: true,
+    },
+  },
+  theme: {},
+  variants: {},
+  plugins: [],
+};
